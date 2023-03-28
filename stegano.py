0f58efa7725b5941f0b1d2d5c9bfaa59df3977f3
kun99 <kuns.km@gmail.com>
2023-03-28 16:00:23 +0700

    final for now

--- stegano.py
+++ stegano.py
@@ -1,7 +1,6 @@
 import numpy as np
 from PIL import Image
 
-# to do: 
 def encode():
     cover_image = Image.open("bird.jpg")
     image_to_hide = Image.open("cat.jpg")
@@ -10,12 +9,11 @@ def encode():
     hidden_image_bytes = np.array(image_to_hide)
     new_image = np.empty_like(np.asarray(cover_image))
     
-    print(image_bytes.shape)
     h = hidden_image_bytes.shape[0]
     w = hidden_image_bytes.shape[1]
     t = hidden_image_bytes.shape[2]
 
-    if(len(image_bytes) > len(hidden_image_bytes)*2):
+    if(len(image_bytes) > len(hidden_image_bytes)):
         for i, nested_array in enumerate(image_bytes):
             for j, array_bytes in enumerate(nested_array):
                 for k, byte in enumerate(array_bytes):
@@ -27,9 +25,10 @@ def encode():
                         changed_byte = np.packbits(cover)
                         new_image[i][j][k] = changed_byte[0]
                     else:
+                        cover[6] = 0
+                        cover[7] = 0
                         new_image[i][j][k] = np.packbits(cover)
         new_image[new_image.shape[0]-1][new_image.shape[1]-1] = [h,w,t]
-        print(new_image[new_image.shape[0]-1][new_image.shape[1]-1])
         save_image = Image.fromarray(new_image)
         save_image.save("got_steganoed.png")
     else:
@@ -38,25 +37,14 @@ def encode():
 def decode():
     decode_image = Image.open("got_steganoed.png")
     decode_bytes = np.array(decode_image)
-    #new_image = np.empty_like(np.asarray(decode_bytes))
-    # h = decode_bytes[decode_bytes.shape[0]-1][decode_bytes.shape[1]-1][0]
-    # w = decode_bytes[decode_bytes.shape[0]-1][decode_bytes.shape[1]-1][1]
-    # t = decode_bytes[decode_bytes.shape[0]-1][decode_bytes.shape[1]-1][2]
-    h = 463
-    w = 703
-    t = 3
-    new_image = np.empty((h,w,t), dtype = np.uint8)
-    print(h, w, t)
+    new_image = np.empty_like(np.asarray(decode_bytes))
     for i, nested_array in enumerate(decode_bytes):
         for j, array_bytes in enumerate(nested_array):
             for k, byte in enumerate(array_bytes):
-                if i < h and j < w and k < t:
-                    binary = np.unpackbits(byte)
-                    new_image[i][j][k] = str(binary[6]) + str(binary[7])
-                # else:
-                #     new_image[i][j][k] =  byte
+                binary = np.unpackbits(byte)
+                new_image[i][j][k] = str(binary[6]) + str(binary[7])
     PILimage = Image.fromarray(new_image)
     PILimage.save("decoded.png")
 
-#encode()
+encode()
 decode()
