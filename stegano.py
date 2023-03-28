c93878cfc8be152d71eb0ec951f0816d6c9dcebe
kun99 <kuns.km@gmail.com>
2023-03-28 16:52:41 +0700

    final ver for project

--- stegano.py
+++ stegano.py
@@ -2,8 +2,8 @@ import numpy as np
 from PIL import Image
 
 def encode():
-    cover_image = Image.open("bird.jpg")
-    image_to_hide = Image.open("cat.jpg")
+    cover_image = Image.open("images/bird.jpg")
+    image_to_hide = Image.open("images/cat.jpg")
     
     image_bytes = np.array(cover_image)
     hidden_image_bytes = np.array(image_to_hide)
@@ -11,33 +11,38 @@ def encode():
     
     h = hidden_image_bytes.shape[0]
     w = hidden_image_bytes.shape[1]
-    t = hidden_image_bytes.shape[2]
+    rgb = hidden_image_bytes.shape[2]
 
     if(len(image_bytes) > len(hidden_image_bytes)):
         for i, nested_array in enumerate(image_bytes):
             for j, array_bytes in enumerate(nested_array):
                 for k, byte in enumerate(array_bytes):
                     cover = np.unpackbits(byte)
-                    if i < h and j < w and k < t:
+                    # replacing two lsbs from byte from cover image
+                    # with two msbs from byte from image to hide
+                    if i < h and j < w and k < rgb:
                         to_hide = np.unpackbits(hidden_image_bytes[i][j][k])
                         cover[6] = to_hide[0]
                         cover[7] = to_hide[1]
                         changed_byte = np.packbits(cover)
                         new_image[i][j][k] = changed_byte[0]
+                    # replacing two lsbs from byte from cover image
+                    # with 0 so when unpacked it is a black pixel
                     else:
                         cover[6] = 0
                         cover[7] = 0
                         new_image[i][j][k] = np.packbits(cover)
-        new_image[new_image.shape[0]-1][new_image.shape[1]-1] = [h,w,t]
         save_image = Image.fromarray(new_image)
-        save_image.save("got_steganoed.png")
+        save_image.save("images/got_steganoed.png")
     else:
         print("cover image too small")
 
 def decode():
-    decode_image = Image.open("got_steganoed.png")
+    decode_image = Image.open("images/got_steganoed.png")
     decode_bytes = np.array(decode_image)
+    
     new_image = np.empty_like(np.asarray(decode_bytes))
+    
     for i, nested_array in enumerate(decode_bytes):
         for j, array_bytes in enumerate(nested_array):
             for k, byte in enumerate(array_bytes):
